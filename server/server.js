66dcce6b92d498240c4e88a5ee2b5481b3e25b96
William Liu <110699040+waileungl@users.noreply.github.com>
2022-12-15 21:20:33 -0800

    new

--- server/server.js
+++ server/server.js
@@ -11,11 +11,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 // Set the Referrer-Policy header in all HTTP responses
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
