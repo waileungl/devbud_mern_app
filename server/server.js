5d9eb98033f3ce45e15035000bf679a118b4817b
William Liu <110699040+waileungl@users.noreply.github.com>
2022-12-15 21:07:09 -0800

    more header added

--- server/server.js
+++ server/server.js
@@ -6,13 +6,16 @@ const DB = 'devBud';
 const app = express()
 const port = 8000
 
-app.use(cors({ origin: '*' }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 // Set the Referrer-Policy header in all HTTP responses
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
