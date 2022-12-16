03426750a3ac964d582ffd779b8e9c6e0f7e86ed
William Liu <110699040+waileungl@users.noreply.github.com>
2022-12-16 09:38:50 -0800

    back to original config

--- server/server.js
+++ server/server.js
@@ -1,16 +1,7 @@
-const fs = require('fs');
-const https = require('https');
 const express = require('express')
 const cors = require('cors');
-const socketio = require('socket.io');
 //mongoDB
 const DB = 'devBud';
-
-const options = {
-    key: fs.readFileSync('path/to/ssl/key'),
-    cert: fs.readFileSync('path/to/ssl/cert')
-};
-
 
 const app = express()
 const port = 8000
@@ -19,18 +10,19 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
+
 // --- CONNECT TO DB USING MONGOOSE ---
 require('./config/mongoose.config')(DB);
 
 // --- IMPORTS ROUTES after teh DB is connected ---
 require('./routes/dev.routes')(app);
 
-const server = https.createServer(options, app);
-server.listen(port, () => console.log(`>>>>listening on port ${port}<<<<`));
 
-const io = socketio(server);
+const server = app.listen(port, () => console.log(`>>>>listening on port ${port}<<<<`))
+
+console.log("this is server>>>>>>>", server);
 // To use socket we have to pass in our server as a param
-// const io = require("socket.io")(server, { cors: true })
+const io = require("socket.io")(server, {cors: true})
 
 // console.log(">>>>>>>>>>IO IS SET UP", io, "--------------------the code above is io")
 
